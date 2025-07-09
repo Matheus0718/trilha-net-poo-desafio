c10df0bf1257b26d173633e9b3c76f8547b32176
Matheus Soares <math.ss0718@gmail.com>
2025-07-08 21:39:16 -0300

    Desafio projeto POO Matheus Soares de Souza

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "1298908-8737", modelo: "N95", imei: "1111-222-333", memoria: 120);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+
+Console.WriteLine("--------------------------------");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "1298829-82892", modelo: "13 Pro MAX", imei: "1111111-2222222", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Ifood");
+
+
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
